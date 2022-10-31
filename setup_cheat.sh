0033884b47c613a5f55b92eab59c4526f076927c
romain dupeux <romain.dupeux24@gmail.com>
2022-11-01 00:41:12 +0100

    fix

--- setup_cheat.sh
+++ setup_cheat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ ! -d ~/.cheat_code_folder ];  then
-    mkdir ~/.cheat_code_folder
+  mkdir ~/.cheat_code_folder
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]] || [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-  curl -s https://github.com/TulipeSoyeuse/cheat_code/tree/main/script
+  curl -s https://github.com/TulipeSoyeuse/cheat_code/tree/main/script > ~/.cheat_code_folder
   pip install -q ~/.cheat_code_folder/script/requirement.txt
   echo "I am ready to sheat"
   echo 'alias cheat="python ~/.cheat_code_folder/script/cheat_script"' >> ~/.aliases
