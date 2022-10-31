5331c9cf71ccd4fa42ed496698aea735ca93c16a
romain dupeux <romain.dupeux24@gmail.com>
2022-11-01 00:10:26 +0100

    init commit

--- setup_cheat.sh
+++ setup_cheat.sh
@@ -4,8 +4,9 @@ if [ ! -d ~/.cheat_code_folder ];  then
     mkdir ~/.cheat_code_folder
 fi
 
-curl -s  >
-chmod u+x 
-pip install -q requirement.txt
-
-echo 'alias check="zsh ~/.cheat_code_folder/"' >> ~/.aliases
+curl -s https://github.com/TulipeSoyeuse/cheat_code/blob/main/setup_cheat.sh
+chmod u+x cheat_CLI.sh
+pip install -q ~/.cheat_code_folder/script/requirement.txt
+echo "I am ready to sheat"
+echo 'alias cheat="python ~/.cheat_code_folder/script/cheat_script"' >> ~/.aliases
+echo "you can now runn 'cheat' command on your terminal to win this code'n'drink ! gl hf"
