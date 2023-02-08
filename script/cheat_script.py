83fab7e894feca2c3644b131bfaa352c5fecfc4f
TulipeSoyeuse <romain.dupeux24@gmail.com>
2023-02-08 18:05:53 +0100

    maj keyboard ready to merge master

--- script/cheat_script.py
+++ script/cheat_script.py
@@ -1,66 +1,54 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 import pyperclip3 as pc
 import numpy as np
 import pandas as pd
 import keyboard
-from pynput.keyboard import Key, Listener
+import subprocess
+import sys
+from pynput.keyboard import Key, Listener, Controller
 from threading import Lock
-
+import time
 lock = Lock()
-
-
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://tournament.lewagon.com/games")
 print("running")
-df = pd.read_csv("script/data/scrapping_solution.csv",index_col=0).dropna().reset_index(drop=True)
+keyboard2 = Controller()
+try :
+    df = pd.read_csv("script/data/scrapping_solution.csv",index_col=0).dropna().reset_index(drop=True)
+except:
+    df =  pd.read_csv("data/scrapping_solution.csv",index_col=0).dropna().reset_index(drop=True)
 a_previous = pc.paste().decode("utf-8")
-paste_special=False
 value_paste=None
 
-
 def on_press(key):
-    global value_paste
-    global paste_special
-    print(value_paste)
-    print(paste_special)
-    if paste_special and key == Key.ctrl:
-        print("go")
-        lock.acquire()
-        keyboard.write(value_paste)
-        lock.release()
-        paste_special = False
-def on_release(key):
-    print('{0} release'.format(key))
-    if key == Key.esc:
-        # Stop listener
-        return False
-
-
-# Collect events until released
-
-listener = Listener(on_press=on_press, on_release=on_release)
-listener.start()
-
-while True:
-
-    a = pc.paste().decode("utf-8")
-
-    if len(a)>0 and a != a_previous:
-        array = np.append(np.array(df.description),a)
+    if key == Key.ctrl :
+        time.sleep(0.3)
+        prompt = driver.find_element(By.XPATH,"//div[@id='instructions']/p").text
+        array = np.append(np.array(df.description),prompt)
         vectorizer = TfidfVectorizer()
         desc_matrix = vectorizer.fit_transform(array)
         cos_sim_matrix = cosine_similarity(desc_matrix, desc_matrix[-1])[:-1]
         index = np.argmax(cos_sim_matrix)
         print("similarity :" + str(cos_sim_matrix[index]))
         soluce = df.loc[index,"solution"]
-        if np.max(cos_sim_matrix[index]) > 0.5 :
-            print(True)
-            pc.copy(soluce)
-            a_previous = soluce
-            paste_special = True
-            value_paste = soluce
-        else :
-            print("no match")
-            pc.copy("no match")
-            paste_special = False
-            value_paste = None
-            a_previous = "no match"
+        # target_code = driver.find_element(By.XPATH,"//div[@class='CodeMirror cm-s-monokai']")
+        # target_code.click()
+        lock.acquire()
+        keyboard.write(soluce)
+        lock.release()
+
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+listener = Listener(on_press=on_press, on_release=on_release)
+listener.start()
+
+while True:
+    pass
